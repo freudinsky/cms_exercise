6202a0c0c5a37b1ec407c75a67e71336ff1b7160
freudinsky <timo.fuchs94@icloud.com>
2023-09-14 11:06:52 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Searchbar from "./Components/Searchbar";
 import Navbar from "./Components/Navbar";
 import Navigation from "./Components/Navigation";
 import Footer from "./Components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
 
-      <Footer />
-    </div>
-  );
 	return (
 		<>
 			{/* <Navbar /> */}

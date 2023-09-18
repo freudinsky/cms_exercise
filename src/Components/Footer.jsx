70dfe7b05dbb41d9cf1fa069f28308a8351409d5
Marta Organisciak <marta.organisciak@wp.pl>
2023-09-18 10:57:18 +0200

    fb,insta,youtube icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>- footer -</div>;
+  return (
+    <>
+      <div class="footer">
+        <p>- footer -</p>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+
+        <div class="footer-icons">
+          <a href="#" class="fa fa-facebook" id="fb-icon"></a>
+          <a href="#" class="fa fa-instagram" id="ig-icon"></a>
+          <a href="#" class="fa fa-youtube-play" id="youtube-icon"></a>
+        </div>
+      </div>
+    </>
+  );
 }

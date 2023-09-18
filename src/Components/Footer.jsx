56bb0ad7bda5678508b9c301b917f319d5eef1ad
Marta Organisciak <marta.organisciak@wp.pl>
2023-09-18 18:11:35 +0200

    navigation_part_styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,33 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
-	return (
-		<>
-			<div class="footer">
-				<ul className="footernav">
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="#">Imprint</Link>
-					</li>
-					<li>
-						<Link to="#">Privacy note</Link>
-					</li>
-				</ul>
-				<ul class="footer-icons">
-					<li>
-						<a href="#" class="fa fa-facebook" id="fb-icon"></a>
-					</li>
+  return (
+    <>
+      <div class="footer">
+        <ul className="footernav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="#">Imprint</Link>
+          </li>
+          <li>
+            <Link to="#">Privacy note</Link>
+          </li>
+        </ul>
+        <div className="popcorn2">
+          <img
+            src="https://icons.iconarchive.com/icons/iconarchive/fat-sugar-food/512/Popcorn-icon.png"
+            width="60"
+          />
+        </div>
 
-					<li>
-						<a href="#" class="fa fa-instagram" id="ig-icon"></a>
-					</li>
-					<li>
-						<a href="#" class="fa fa-youtube-play" id="youtube-icon"></a>
-					</li>
-				</ul>
-			</div>
-		</>
-	);
+        <ul className="footer-icons">
+          <li>
+            <a href="#" class="fa fa-facebook" id="fb-icon"></a>
+          </li>
+
+          <li>
+            <a href="#" class="fa fa-instagram" id="ig-icon"></a>
+          </li>
+          <li>
+            <a href="#" class="fa fa-youtube-play" id="youtube-icon"></a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -2,28 +2,27 @@ import { Link } from "react-router-dom";
 import { limitWords } from "../serivces/service";
 
 export default function PostCard({ post }) {
-	
-	const limitedHeading = limitWords(post.fields.heading, 7);
-	const limitedText = limitWords(
-		post.fields.postText.content[0].content[0].value,
-		20
-	);
+  const limitedHeading = limitWords(post.fields.heading, 7);
+  const limitedText = limitWords(
+    post.fields.postText.content[0].content[0].value,
+    20
+  );
 
-	return (
-		<>
-			<Link to={`/post/${post.sys.id}`} className="card-link">
-				<div className="card text-center">
-					<img
-						src={post.fields.imgUrl}
-						className="card-img-top"
-						alt={post.fields.heading}
-					/>
-					<div className="card-body">
-						<h5 className="card-title">{limitedHeading}</h5>
-						<p className="card-text">{limitedText}</p>
-					</div>
-				</div>
-			</Link>
-		</>
-	);
+  return (
+    <>
+      <Link to={`/post/${post.sys.id}`} className="card-link">
+        <div className="card text-center">
+          <img
+            src={post.fields.imgUrl}
+            className="card-img-top"
+            alt={post.fields.heading}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{limitedHeading}</h5>
+            <p className="card-text">{limitedText}</p>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
 }
